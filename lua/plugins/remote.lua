f97fee273b968cf5bf32446dac918bdddbca1271
Denis <dbreznikov@gmail.com>
2024-11-21 23:06:23 +0300

    feat: add remote plugins

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,17 @@
+return {
+  -- {
+  --   'chipsenkbeil/distant.nvim',
+  --   branch = 'v0.3',
+  --   config = function()
+  --     require('distant'):setup()
+  --   end,
+  -- },
+  --
+  -- {
+  -- 'miversen33/netman.nvim'
+  -- }
+  --
+  -- {
+  -- 'jamestthompson3/nvim-remote-containers'
+  -- }
+}
